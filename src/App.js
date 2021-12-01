4d5f8efa0f7fa764688d31b72d189568d66ea921
Oleksandr Korniiets <alekskor7@gmail.com>
2021-12-01 20:20:31 +0200

    React Router
    React Router

--- src/App.js
+++ src/App.js
@@ -1,87 +1,63 @@
-import { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import Loader from 'react-loader-spinner';
-import Searchbar from './components/Searchbar/Searchbar';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import { fetchImagesByQuery, IMAGES_PER_PAGE } from './Api/Api';
-import Button from './components/Button/Button';
-import Style from "./App.module.css";
+import { ToastContainer } from 'react-toastify';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Container from './components/Container';
+import ErrorPage from './components/ErrorPage';
 
-const Status = {
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-  NEED_LOADING: 'needLoading',
-};
+const HomePage = lazy(() =>
+  import('./views/HomePage' /*webpackChunkName: "home-page" */),
+);
+const MoviesPage = lazy(() =>
+  import('./views/MoviesPage' /*webpackChunkName: "movies-page" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    './views/MovieDetailsPage' /*webpackChunkName: "movie-details-page" */
+  ),
+);
 
-export default function App() {
-  const [query, setQuery] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isMoreAvailable, setIsMoreAvailable] = useState(false);
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState(null);
-
-  useEffect(() => {
-    if (status === Status.NEED_LOADING) {
-      setStatus(Status.PENDING);
-
-      fetchImagesByQuery(query, page)
-        .then(results => {
-          const resultsCount = results.hits.length;
-          if (resultsCount === 0) {
-            setError(new Error(`No search results for ${query}`));
-            setStatus(Status.REJECTED);
-            return;
-          }
-
-          setImages(prev => [...images, ...results.hits]);
-          setIsMoreAvailable(checkAvailability(resultsCount));
-          setStatus(Status.RESOLVED);
-        })
-        .then(() => {
-          if (page !== 1) {
-            window.scrollTo({
-              top: document.documentElement.scrollHeight,
-              behavior: 'smooth',
-            });
-          }
-        })
-        .catch(error => {
-          setError(error);
-          setStatus(Status.REJECTED);
-        });
-    }
-  }, [images, page, query, status]);
-
-  const handleSearchSubmit = query => {
-    setImages([]);
-    setQuery(query);
-    setPage(1);
-    setStatus(Status.NEED_LOADING);
-  };
-
-  const handleLoadMore = () => {
-    setPage(prev => prev + 1);
-    setStatus(Status.NEED_LOADING);
-  };
-
-  const checkAvailability = itemsLength => {
-    return !(itemsLength < IMAGES_PER_PAGE);
-  };
-
+function App() {
   return (
-    <div className={Style.App}>
-      <Searchbar onSubmit={handleSearchSubmit} />
-      {status === Status.REJECTED && <div>{error.message}</div>}
-      <ImageGallery items={images} />
-      {status === Status.PENDING && (
-        <div className={Style.Loader}>
+    <>
+      <Container>
+        <Header>
+          <Navigation />
+        </Header>
+        <Suspense
+          fallback={
           <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
-        </div>
-      )}
-      {status === Status.RESOLVED && isMoreAvailable && (
-        <Button onClick={handleLoadMore}>Load more</Button>
-      )}
-    </div>
+          }
+        >
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+
+            <Route path="/movies" exact>
+              <MoviesPage />
+            </Route>
+
+            <Route path="/movies/:movieId">
+              <MovieDetailsPage />
+            </Route>
+
+            <Route>
+              <ErrorPage />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Container>
+      <ToastContainer
+        autoClose={3000}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+      />
+    </>
   );
 }
+
+export default App;
